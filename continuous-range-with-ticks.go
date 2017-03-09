6c8893f1cceceedb34daeb93f767d4a3ff5f0dfe
Arne Hilmann <arne.hilmann@gmail.com>
2017-03-09 10:55:27 +0100

    drop dependency to rrdtool, create a multiframe gif with different linespacings

--- continuous-range-with-ticks.go
+++ continuous-range-with-ticks.go
@@ -1,15 +1,19 @@
 package chartcontrib
 
 import (
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/math/fixed"
 	"log"
 	"math"
 
 	"github.com/wcharczuk/go-chart"
+	"github.com/wcharczuk/go-chart/drawing"
 )
 
 type MyRange struct {
 	*chart.ContinuousRange
-	count int
+	count       int
+	linespacing float64
 }
 
 func (r MyRange) GetTicks(re chart.Renderer, defaults chart.Style, vf chart.ValueFormatter) []chart.Tick {
@@ -19,7 +23,31 @@ func (r MyRange) GetTicks(re chart.Renderer, defaults chart.Style, vf chart.Valu
 	log.Println(r.GetMin())
 	log.Println(r.GetMax())
 
-	steplength := r.GetDelta() / float64(r.count)
+	count := r.count
+	if count == 0 {
+		log.Println("count is zero")
+		log.Println("domain", r.GetDomain())
+		log.Println("font", defaults.GetFont().Name(truetype.NameIDFontFullName))
+		log.Println("size", defaults.GetFontSize())
+		log.Println("bounds", defaults.GetFont().Bounds(fixed.Int26_6(defaults.GetFontSize())))
+		font_height := float64(defaults.GetFont().Bounds(fixed.Int26_6(defaults.GetFontSize())).Max.Y)
+		log.Println("font height", font_height)
+
+		log.Println(float64(r.GetDomain()) / float64(font_height))
+		log.Println(float64(r.GetDomain()) / float64(font_height) / r.linespacing)
+		count = int(float64(r.GetDomain()) / float64(font_height) / r.linespacing)
+		log.Println("count", count)
+
+		extents := drawing.Extents(defaults.GetFont(), defaults.GetFontSize())
+		log.Println("ascent", extents.Ascent)
+		log.Println("descent", extents.Descent)
+		log.Println("height", extents.Height)
+
+		count = int(float64(r.GetDomain()) / (float64(extents.Height) * r.linespacing))
+		log.Println("count", count)
+	}
+
+	steplength := r.GetDelta() / float64(count)
 	log.Println(steplength)
 
 	factor := math.Pow10(int(math.Log10(steplength)))
@@ -28,17 +56,17 @@ func (r MyRange) GetTicks(re chart.Renderer, defaults chart.Style, vf chart.Valu
 	normlength := steplength / factor
 	log.Println(normlength)
 
-	var minindex int
+	var minindex float64
 	mindiff := math.Inf(1)
-	for _, steptry := range []int{1, 2, 5, 10} {
-		diff := math.Abs(normlength - float64(steptry))
-		log.Println(diff)
+	for _, steptry := range []float64{.1, .2, .5, 1, 2, 2.5, 5, 10} {
+		diff := math.Abs(normlength - steptry)
+		log.Println("diff", steptry, diff)
 		if diff < mindiff {
 			mindiff = diff
 			minindex = steptry
 		}
 	}
-	log.Println(minindex)
+	log.Println("min", minindex)
 
 	newsteplength := float64(minindex) * factor
 	log.Println(newsteplength)
@@ -49,12 +77,16 @@ func (r MyRange) GetTicks(re chart.Renderer, defaults chart.Style, vf chart.Valu
 	ticks := []chart.Tick{}
 	for actual := chart.Math.RoundUp(min, newsteplength); actual <= max; actual += newsteplength {
 		value := float64(actual)
-        ticks = append(ticks, chart.Tick{Value: value, Label: vf(value)})
+		ticks = append(ticks, chart.Tick{Value: value, Label: vf(value)})
 	}
 
 	return ticks
 }
 
-func ContinuousRangeWithTicks(count int) MyRange {
+func ContinuousRangeWithTicksLinespacing(linespacing float64) MyRange {
+	return MyRange{ContinuousRange: &chart.ContinuousRange{}, linespacing: linespacing}
+}
+
+func ContinuousRangeWithTicksCount(count int) MyRange {
 	return MyRange{ContinuousRange: &chart.ContinuousRange{}, count: count}
 }
--- go-chart-contrib_test.go
+++ go-chart-contrib_test.go
@@ -3,15 +3,20 @@ package chartcontrib_test
 import "testing"
 
 import (
-	"io/ioutil"
+	"bufio"
+	"fmt"
+	"image"
+	"image/gif"
+	"image/png"
 	"log"
 	"math"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/andybons/gogif"
 
 	"github.com/wcharczuk/go-chart"
 	"github.com/wcharczuk/go-chart/drawing"
@@ -31,48 +36,10 @@ func NewTimeline(name string) Timeline {
 	return Timeline{name, []time.Time{}, []float64{}}
 }
 
-func TestScale(t *testing.T) {
-	out, err := exec.Command("rrdtool", "fetch", "testdata/temperature.rrd", "AVERAGE",
-		"-s", "02/22/2017", "-e", "02/24/2017").Output()
-	PanicIf(err)
-
-	var timelines []Timeline
-	for _, line := range strings.Split(string(out), "\n") {
-		if line == "" {
-			continue
-		}
-		fields := strings.Fields(line)
-		if len(timelines) == 0 {
-			for _, name := range fields {
-				timelines = append(timelines, NewTimeline(name))
-			}
-			continue
-		}
-		epoch, err := strconv.Atoi(strings.TrimSuffix(fields[0], ":"))
-		PanicIf(err)
-		for index, field := range fields[1:] {
-			value, err := strconv.ParseFloat(field, 32)
-			PanicIf(err)
-			if math.IsNaN(value) {
-				continue
-			}
-			timelines[index].epochs = append(timelines[index].epochs, time.Unix(int64(epoch), 0))
-			timelines[index].values = append(timelines[index].values, float64(value))
-		}
-	}
-	for _, line := range timelines {
-		log.Println(line.name)
-		for i := range line.epochs {
-			log.Println(line.epochs[i], line.values[i])
-		}
-		break
-	}
-
-	log.Println("assembling graph")
-
-	graph := chart.Chart{
-		Width:  600,
-		Height: 400,
+func createChart(timelines []Timeline, linespacing float64) (chart.Chart, error) {
+	c := chart.Chart{
+		Width:  800,
+		Height: 600,
 		XAxis: chart.XAxis{
 			Style: chart.Style{
 				Show: true,
@@ -98,7 +65,11 @@ func TestScale(t *testing.T) {
 			ValueFormatter: func(v interface{}) string {
 				return chart.FloatValueFormatterWithFormat(v, "%0.1f")
 			},
-			Range: chartcontrib.ContinuousRangeWithTicks(5),
+			Range: chartcontrib.ContinuousRangeWithTicksLinespacing(linespacing),
+			Name:  fmt.Sprintf("linespacing: %.1f", linespacing),
+			NameStyle: chart.Style{
+				Show: true,
+			},
 		},
 		Series: []chart.Series{
 			chart.TimeSeries{
@@ -124,24 +95,93 @@ func TestScale(t *testing.T) {
 			},
 		},
 	}
-	err = graph.Series[0].Validate()
-	PanicIf(err)
-	err = graph.Series[1].Validate()
-	PanicIf(err)
+	if err := c.Series[0].Validate(); err != nil {
+		return chart.Chart{}, err
+	}
+	if err := c.Series[1].Validate(); err != nil {
+		return chart.Chart{}, err
+	}
+	return c, nil
+}
 
-	log.Println("tmp dir", os.TempDir())
-	prefix := "go-chart-contrib-test-"
-	old_charts, err := filepath.Glob(os.TempDir() + prefix + "*")
+func TestScale(t *testing.T) {
+	file, err := os.Open("testdata/temperature.dump")
 	PanicIf(err)
-	for _, old_chart := range old_charts {
-		log.Println("old", old_chart)
-		err := os.Remove(old_chart)
+	defer file.Close()
+
+	var timelines []Timeline
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+		fields := strings.Fields(line)
+		if len(timelines) == 0 {
+			for _, name := range fields {
+				timelines = append(timelines, NewTimeline(name))
+			}
+			continue
+		}
+		epoch, err := strconv.Atoi(strings.TrimSuffix(fields[0], ":"))
 		PanicIf(err)
+		for index, field := range fields[1:] {
+			value, err := strconv.ParseFloat(field, 32)
+			PanicIf(err)
+			if math.IsNaN(value) {
+				continue
+			}
+			timelines[index].epochs = append(timelines[index].epochs, time.Unix(int64(epoch), 0))
+			timelines[index].values = append(timelines[index].values, float64(value))
+		}
+	}
+	err = scanner.Err()
+	PanicIf(err)
+	for _, line := range timelines {
+		log.Println(line.name)
+		for i := range line.epochs {
+			log.Println(line.epochs[i], line.values[i])
+		}
+		break
 	}
 
-	f, err := ioutil.TempFile("", prefix)
+	var filenames []string
+	for i := 1; i <= 10; i++ {
+		c, err := createChart(timelines, float64(i))
+		PanicIf(err)
+		log.Println("assembling chart with linespacing", i)
+		filename := filepath.Join(os.TempDir(), fmt.Sprintf("go-chart-contrib-test.%d.png", i))
+		filenames = append(filenames, filename)
+		f, err := os.Create(filepath.Join(os.TempDir(), fmt.Sprintf("go-chart-contrib-test.%d.png", i)))
+		PanicIf(err)
+		defer f.Close()
+		c.Render(chart.PNG, f)
+		log.Println("chart can be found in", f.Name())
+	}
+
+	outGif := &gif.GIF{}
+	for i := len(filenames) - 1; i >= 0; i-- {
+		filename := filenames[i]
+		f, err := os.Open(filename)
+		PanicIf(err)
+		defer f.Close()
+		simage, err := png.Decode(f)
+		PanicIf(err)
+
+		bounds := simage.Bounds()
+		palettedImage := image.NewPaletted(bounds, nil)
+		quantizer := gogif.MedianCutQuantizer{NumColor: 64}
+		quantizer.Quantize(palettedImage, bounds, simage, image.ZP)
+
+		outGif.Image = append(outGif.Image, palettedImage)
+		outGif.Delay = append(outGif.Delay, 1)
+	}
+
+	// save to out.gif
+	filename := filepath.Join(os.TempDir(), "go-chart-contrib-test.all.png")
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0600)
 	PanicIf(err)
 	defer f.Close()
-	graph.Render(chart.PNG, f)
-	log.Println("graph can be found in", f.Name())
+	gif.EncodeAll(f, outGif)
+	log.Println("all charts can be found in", f.Name())
 }
